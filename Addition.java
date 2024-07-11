26eaa6fb3ff0b382423a867e6fd4e021cfd8e345
chalavadinagamani <chalavadinagamani123@gamil.com>
2024-07-11 16:43:54 +0530

    first model

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+public Static void main(String args[])
+{
+int n1=30;
+int n2=54;
+Addition=n1+n2;
+System.out.println("the Addition of twonumber");
+}
+}
---
+++ positive.java
@@ -0,0 +1,15 @@
+public class positive{
+public Static main (String args[])
+{
+int n1=30;
+int n2=54;
+
+if(n%2==0)
+{
+
+System.out.println("the positive number");
+}
+
+}
+
+}
